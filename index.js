e5795118599fc29448322e05ea8875047c6839f8
mdowns23 <95893859+mdowns23@users.noreply.github.com>
2024-08-05 21:32:26 -0700

    added api url from lambda func made in terraform

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const counter = document.querySelector(".count");
 async function updateCounter() {
     // fetch response
     let response = await fetch(
-        "https://nea4tns5e6656i4usw2uyhflqa0omldb.lambda-url.us-west-2.on.aws/"
+        "https://dgipwo4zvirfmbxmpdr7r55mta0rmoek.lambda-url.us-west-2.on.aws/"
     );
     //get viewer count and update
     let data = await response.json();

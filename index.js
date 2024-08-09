9e874f97d986242ed556f6673880a12c633101a0
mdowns23 <95893859+mdowns23@users.noreply.github.com>
2024-08-08 21:52:37 -0700

    updated lambda link on index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const counter = document.querySelector(".count");
 async function updateCounter() {
     // fetch response
     let response = await fetch(
-        "https://dgipwo4zvirfmbxmpdr7r55mta0rmoek.lambda-url.us-west-2.on.aws/"
+        "https://a3kfxebkqukl7zq5jsjmgmgjra0qvsjs.lambda-url.us-west-2.on.aws/"
     );
     //get viewer count and update
     let data = await response.json();
